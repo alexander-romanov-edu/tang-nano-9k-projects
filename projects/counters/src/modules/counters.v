5e90f3dd073872d09ba6c422fde04602938481e0
Sergei Zimmerman <52955378+serjzimmerman@users.noreply.github.com>
2025-03-07 18:39:20 +0300

    feat: freqcounter top

--- projects/counters/src/modules/counters.v
+++ projects/counters/src/modules/counters.v
@@ -10,13 +10,15 @@ module counters #(
     output wire [15:0] dat
 );
   wire ce1ms;
+  reg [20:0] freq_decimal;
+
   sevenseg_display #(
       .CLKFREQ(CLKFREQ)
   ) display (
       .clk(clk),
       .ce1ms(ce1ms),
       .ptr(switch[5:4]),
-      .dat(dat[15:0]),
+      .dat(freq_decimal),
       .an(digits),
       .seg(segments)
   );
@@ -31,54 +33,18 @@ module counters #(
       .ce  (ce1ms)
   );
 
-  wire btn1_debounced;
-  debouncer debouncer (
-      .clk(clk),
-      .ce1ms(ce1ms),
-      .btn_in(btn1),
-      .btn_out(btn1_debounced)
+  reg [15:0] freq;
+  freqcounter #(
+      .WIDTH(16)
+  ) freqcounter (
+      .ce  (ce1ms),
+      .clk (clk),
+      .ovf (ce1s_n_ms),
+      .freq(freq)
   );
 
-  wire vcb4re_ceo;
-  vcb_m_re #(
-      .WIDTH(4)
-  ) vcb4re (
-      .ce (btn1_debounced),
-      .clk(clk),
-      .rst(rst),
-      .q  (dat[3:0]),
-      .ceo(vcb4re_ceo)
-  );
-
-  wire vcbd4se_ceo;
-  vcbd_m_se #(
-      .WIDTH(4)
-  ) vcbd4se (
-      .ce (btn1_debounced),
-      .clk(clk),
-      .rst(rst),
-      .q  (dat[7:4]),
-      .ceo(vcbd4se_ceo)
-  );
-
-  wire vcd_re_ceo;
-  vcd_re vcd_re (
-      .ce (btn1_debounced),
-      .clk(clk),
-      .rst(rst),
-      .q  (dat[11:8]),
-      .ceo(vcd_re_ceo)
-  );
-
-  vcb_m_cled #(
-      .WIDTH(4)
-  ) vcb_m_cled (
-      .ce (btn1_debounced),
-      .up (switch[6]),
-      .di (switch[3:0]),
-      .l  (1'b0),
-      .clk(clk),
-      .clr(rst),
-      .q  (dat[15:12])
+  bin2bcd bin2bcd (
+      .bin(freq),
+      .bcd(freq_decimal)
   );
 endmodule
